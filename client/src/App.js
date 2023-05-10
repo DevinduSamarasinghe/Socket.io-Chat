b9130785d0f28073e1ace3b72ddad33978c1d733
DevinduSamarasinghe <devindu990@gmail.com>
2023-05-10 20:12:01 +0530

    +route passes and base implementaion

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import Chats from './pages/Chats';
+
 import './App.css';
 import { Button } from '@chakra-ui/react';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Chats from './pages/Chats';
 import { Link } from 'react-router-dom';
 
 function App() {
